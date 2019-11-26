0021e37ede22a5ced1fd1684ec43c9e587d17526
rodrigobutta <rbutta@gmail.com>
2019-11-26 20:15:15 -0300

    restufl empty methods in game controller

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+
+    public function getAllGames()
+    {
+        // get all Games
+    }
+
+    public function createGame(Request $request)
+    {
+        // create a Game
+    }
+
+    public function getGame($id)
+    {
+        // get a Game
+    }
+
+    public function updateGame(Request $request, $id)
+    {
+        // update a Game
+    }
+
+    public function deleteGame($id)
+    {
+        // delete a Game
+    }
+
 }
