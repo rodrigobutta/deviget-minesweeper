7a1a2d3ef3aaaef5810fb8242fbe27be24254be6
rodrigobutta <rbutta@gmail.com>
2019-11-26 19:55:25 -0300

    game migration (with intention of game level next)

--- database/migrations/2019_11_26_224221_create_games_table.php
+++ database/migrations/2019_11_26_224221_create_games_table.php
@@ -13,8 +13,14 @@ class CreateGamesTable extends Migration
      */
     public function up()
     {
+      
         Schema::create('games', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->engine = 'InnoDB';
+            $table->uuid('id')->primary();                  // i'll try to use uuid instead of incremental, it's very handy this times with layered patterns and all that
+            $table->integer('user_id')->unsigned();         // relation to the session user
+            $table->integer('level_id')->unsigned();        // i´ll use some level to group row, cols, mines relation
+            $table->boolean('won')->default(0);             // flag of the resuklt
+            $table->timestamp('ended_at')->nullable();      // date the game ended (won or lost)
             $table->timestamps();
         });
     }
