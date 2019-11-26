373e6bf45279e20ca4de33a6c98d814b4ae84321
rodrigobutta <rbutta@gmail.com>
2019-11-26 20:06:25 -0300

    GAME model with with uuid and Level and User relations

--- app/Game.php
+++ app/Game.php
@@ -6,5 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    protected $guarded = []; 
+
+
+    // change incremental ID for UUID
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($game) {
+            $game->{$game->getKeyName()} = (string) Str::uuid();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    // END change incremental ID for UUDI
+
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function level()
+    {
+        return $this->belongsTo('App\Level');
+    }
+
 }
